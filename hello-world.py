c844010c15d54f7e3cb3739341b3e27aae2718e3
Gopal Amlekar <gopal.amlekar@gmail.com>
2017-09-12 01:50:25 +0100

    Adding Hello world code

---
+++ hello-world.py
@@ -0,0 +1,7 @@
+# A simple Python program to print hello <your name>
+
+print "Welcome to Python on Raspberry Pi\n"
+
+name = raw_input ("Please tell me your name and press Enter key\n")
+
+print "Hello", name
